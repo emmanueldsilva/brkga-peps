0b51a0289d84bc1d3d73fc4d953a891fe7580a2c
Emmanuel <emmanueldsilva@github.com>
2016-03-20 16:03:32 -0300

    Alteração da classe Main para comportar mais benchmarks

--- src/uem/br/brkga/peps/main/Main.java
+++ src/uem/br/brkga/peps/main/Main.java
@@ -43,16 +43,16 @@ public class Main {
 		parametrosAlgoritmo.setTamanhoGrupoElite(40.0);
 		parametrosAlgoritmo.setTamanhoGrupoMutantes(30.0);
 		parametrosAlgoritmo.setProbabilidadeHerancaElite(65.0);
-//		parametrosAlgoritmo.setPathBenchmark("/home/emmanuel/projetos/ag-peps/resources/problem-generator/inst10-5-10-5.conf");
-//		parametrosAlgoritmo.setPathBenchmark("/home/emmanuel/projetos/ag-peps/resources/problem-generator/inst10-10-10-5.conf");
-//		parametrosAlgoritmo.setPathBenchmark("/home/emmanuel/projetos/ag-peps/resources/problem-generator/inst10-15-10-5.conf");
-		parametrosAlgoritmo.setPathBenchmark("/home/emmanuel/projetos/ag-peps/resources/problem-generator/inst20-5-10-5.conf");
-//		parametrosAlgoritmo.setPathBenchmark("/home/emmanuel/projetos/ag-peps/resources/problem-generator/inst20-10-10-5.conf");
-//		parametrosAlgoritmo.setPathBenchmark("/home/emmanuel/projetos/ag-peps/resources/problem-generator/inst20-15-10-5.conf");
-//		parametrosAlgoritmo.setPathBenchmark("/home/emmanuel/projetos/ag-peps/resources/problem-generator/inst30-15-10-5.conf");
-//		parametrosAlgoritmo.setPathBenchmark("/home/emmanuel/projetos/ag-peps/resources/problem-generator/inst30-5-10-5.conf");
-//		parametrosAlgoritmo.setPathBenchmark("/home/emmanuel/projetos/ag-peps/resources/problem-generator/inst30-10d-10-5.conf");
-//		parametrosAlgoritmo.setPathBenchmark("/home/emmanuel/projetos/ag-peps/resources/problem-generator/inst30-15-10-5.conf");
+//		parametrosAlgoritmo.setPathBenchmark(System.getProperty("user.dir") + "/resources/problem-generator/inst10-5-10-5.conf");
+//		parametrosAlgoritmo.setPathBenchmark(System.getProperty("user.dir") + "/resources/problem-generator/inst10-10-10-5.conf");
+//		parametrosAlgoritmo.setPathBenchmark(System.getProperty("user.dir") + "/resources/problem-generator/inst10-15-10-5.conf");
+		parametrosAlgoritmo.setPathBenchmark(System.getProperty("user.dir") + "/resources/problem-generator/inst20-5-10-5.conf");
+//		parametrosAlgoritmo.setPathBenchmark(System.getProperty("user.dir") + "/resources/problem-generator/inst20-10-10-5.conf");
+//		parametrosAlgoritmo.setPathBenchmark(System.getProperty("user.dir") + "/resources/problem-generator/inst20-15-10-5.conf");
+//		parametrosAlgoritmo.setPathBenchmark(System.getProperty("user.dir") + "/resources/problem-generator/inst30-15-10-5.conf");
+//		parametrosAlgoritmo.setPathBenchmark(System.getProperty("user.dir") + "/resources/problem-generator/inst30-5-10-5.conf");
+//		parametrosAlgoritmo.setPathBenchmark(System.getProperty("user.dir") + "/resources/problem-generator/inst30-10-10-5.conf");
+//		parametrosAlgoritmo.setPathBenchmark(System.getProperty("user.dir") + "/resources/problem-generator/inst30-15-10-5.conf");
 		return parametrosAlgoritmo;
 	}
 	
